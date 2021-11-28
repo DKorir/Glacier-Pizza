ad7a0ff40297de878a27dbe72cc685ad87efaffe
DKorir <dominic.korir@student.moringaschool.com>
2021-11-28 17:18:59 +0300

    initial add prototypes for functions crust,pizza, and topping

--- script.js
+++ script.js
@@ -1,32 +1,90 @@
-//business logic
-// object function
-function selectedPizzaSize(size, crust, topping){
-    this.selectSize=size
+// //business logic
+// // object function
+// function PizzaSize(name){
+//     this.FirstName=name
+// }
+// //pizza prototype
+// // PizzaSize.prototype.pizzaName=function(){
+// //     return this.FirstName;
+// // }
+
+
+
+// $(document).ready(function(){
+//     $(document).on("click", "#btn", function(event){
+//         event.preventDefault()
+//         var size=$("#size option:selected").text();//choice of user's pizza size
+//        var crust=$("#crust option:selected").text();//choice of user's crust type
+//        var topping=$("#topping option:selected").text();//choice of user's topping type
+        
+
+//         //prices
+//         var sizePrice=parseInt($("#size option:selected").val())//choice of user's pizza price
+//        var crustPrice=parseInt($("#crust option:selected").val())//choice of user's crust price
+//        var toppingPrice=parseInt($("#topping option:selected").val())//choice of user's topping price
+
+//        var quantity= $("#qty").val();//units of pizza ordered
+//         //total bill * quantity
+//        var totalBill=(sizePrice+crustPrice+toppingPrice)*quantity;
+//         // alert(totalBill);
+
+
+        
+//         var obj1= new PizzaSize(size)
+//         var crust= new crustType(crustName)
+//         var topping= new toppingName(size)        
+        
+//         //append
+//         $("#shopping").append("<li>"+obj1.FirstName+ "</li>")
+//     })
+// })
+
+function PizzaSize(size){
+    this.selectPizza=size
+}
+//prototypes
+PizzaSize.prototype.pizza=function(){
+    return this.selectPizza;
+}
+function CrustType(crust){
     this.selectCrust=crust
+}
+//prototypes
+CrustType.prototype.crust=function(){
+    return this.selectCrust;
+}
+function ToppingType(topping){
     this.selectTopping=topping
 }
-//pizza prototype
-
+//prototypes
+CrustType.prototype.crust=function(){
+    return this.selectCrust;
+}
 
 
 $(document).ready(function(){
     $(document).on("click", "#btn", function(event){
         event.preventDefault()
-        var size=$("#size option:selected").text();//choice of user's pizza size
-       var crust=$("#crust option:selected").text();//choice of user's crust type
-       var topping=$("#topping option:selected").text();//choice of user's topping type
-        
-
+        var size=$("#size option:selected").text()//choice of user's pizza size
+        var crust=$("#crust option:selected").text();//choice of user's crust type
+        var topping=$("#topping option:selected").text();//choice of user's topping type
         //prices
-        var sizePrice=$("#size option:selected").val();//choice of user's pizza price
-       var crustPrice=$("#crust option:selected").val();//choice of user's crust price
-       var toppingPrice=$("#topping option:selected").val();//choice of user's topping price
-       alert(sizePrice);
-
-
+        var sizePrice=parseInt($("#size option:selected").val())//choice of user's pizza price
+        var crustPrice=parseInt($("#crust option:selected").val())//choice of user's crust price
+        var toppingPrice=parseInt($("#topping option:selected").val())//choice of user's topping price
         var quantity= $("#qty").val();//units of pizza ordered
-        var object1= new PizzaSize(size+crust+topping)
-        //append
-        $(".pizza-language").append("<p>"+ object1+ "</p>")
+        
+        //total bill * quantity
+        var totalBill=(sizePrice+crustPrice+toppingPrice)*quantity;
+
+
+
+        // alert(input)
+    
+        var obj1=new PizzaSize(size)
+        var obj2=new CrustType(crust)
+        var obj3=new ToppingType(topping)
+
+        $("#shopping").html("<li>"+"Your Pizza size is:- "+obj1.selectPizza+"<br>"+"Your choosen Crust Type is:-"+obj2.selectCrust+"<br>"+object3.selectTopping+ "</li>")
     })
 })
